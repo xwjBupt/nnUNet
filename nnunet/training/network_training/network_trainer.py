e7296dc4fb543b82ba26f962b700caab33ebff1f
xwjBupt <xwj_bupt@163.com>
2022-11-24 14:59:53 +0800

    change back to its own log

--- nnunet/training/network_training/network_trainer.py
+++ nnunet/training/network_training/network_trainer.py
@@ -248,53 +248,55 @@ class NetworkTrainer(object):
             self.print_to_log_file("failed to plot: ", sys.exc_info())
 
     def print_to_log_file(self, *args, also_print_to_console=True, add_timestamp=True):
-        log_file = join(self.output_folder, "LOG.log")
-        logger.add(log_file)
-        logger.info(*args)
+        # log_file = join(self.output_folder, "LOG.log")
+        # logger.add(log_file)
+        # for a in args:
+        #     logger.info(str(a) + " ")
+        # logger.info("\n")
 
-        # timestamp = time()
-        # dt_object = datetime.fromtimestamp(timestamp)
+        timestamp = time()
+        dt_object = datetime.fromtimestamp(timestamp)
 
-        # if add_timestamp:
-        #     args = ("%s:" % dt_object, *args)
+        if add_timestamp:
+            args = ("%s:" % dt_object, *args)
 
-        # if self.log_file is None:
-        #     maybe_mkdir_p(self.output_folder)
-        #     timestamp = datetime.now()
-        #     self.log_file = join(
-        #         self.output_folder,
-        #         "training_log_%d_%d_%d_%02.0d_%02.0d_%02.0d.txt"
-        #         % (
-        #             timestamp.year,
-        #             timestamp.month,
-        #             timestamp.day,
-        #             timestamp.hour,
-        #             timestamp.minute,
-        #             timestamp.second,
-        #         ),
-        #     )
-        #     with open(self.log_file, "w") as f:
-        #         f.write("Starting... \n")
-        # successful = False
-        # max_attempts = 5
-        # ctr = 0
-        # while not successful and ctr < max_attempts:
-        #     try:
-        #         with open(self.log_file, "a+") as f:
-        #             for a in args:
-        #                 f.write(str(a))
-        #                 f.write(" ")
-        #             f.write("\n")
-        #         successful = True
-        #     except IOError:
-        #         print(
-        #             "%s: failed to log: " % datetime.fromtimestamp(timestamp),
-        #             sys.exc_info(),
-        #         )
-        #         sleep(0.5)
-        #         ctr += 1
-        # if also_print_to_console:
-        #     print(*args)
+        if self.log_file is None:
+            maybe_mkdir_p(self.output_folder)
+            timestamp = datetime.now()
+            self.log_file = join(
+                self.output_folder,
+                "training_log_%d_%d_%d_%02.0d_%02.0d_%02.0d.txt"
+                % (
+                    timestamp.year,
+                    timestamp.month,
+                    timestamp.day,
+                    timestamp.hour,
+                    timestamp.minute,
+                    timestamp.second,
+                ),
+            )
+            with open(self.log_file, "w") as f:
+                f.write("Starting... \n")
+        successful = False
+        max_attempts = 5
+        ctr = 0
+        while not successful and ctr < max_attempts:
+            try:
+                with open(self.log_file, "a+") as f:
+                    for a in args:
+                        f.write(str(a))
+                        f.write(" ")
+                    f.write("\n")
+                successful = True
+            except IOError:
+                print(
+                    "%s: failed to log: " % datetime.fromtimestamp(timestamp),
+                    sys.exc_info(),
+                )
+                sleep(0.5)
+                ctr += 1
+        if also_print_to_console:
+            print(*args)
 
     def save_checkpoint(self, fname, save_optimizer=True):
         start_time = time()

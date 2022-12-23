dc58bcd011fec3635b7c082b621fbe54f8ae5f07
xwjBupt <xwj_bupt@163.com>
2022-12-23 19:38:25 +0800

    NOT IN DEBUG, Commit INFO >>> 12_23-19_38# TASKID-512 #ERNET-wDS-epoch500-w_skip_decseg-E7_D7 <<<

--- nnunet/training/network_training/nnUNetTrainerV2.py
+++ nnunet/training/network_training/nnUNetTrainerV2.py
@@ -598,7 +598,7 @@ class nnUNetTrainerV2(nnUNetTrainer):
 
 
 def get_FP_FN_label(
-    img, target, use_scales=[2], raw_target_size=(256, 256), ero=7, dil=3
+    img, target, use_scales=[2], raw_target_size=(256, 256), ero=7, dil=7
 ):
 
     t0 = target[0]

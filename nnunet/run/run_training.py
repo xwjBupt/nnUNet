25b4ec8317eceb99341a40829b131685fbbe89d1
xwjBupt <xwj_bupt@163.com>
2022-12-26 13:49:22 +0800

    Commit INFO >>> 12_26-13_49# TASKID-512 #ERNET-wDS-epoch500-baseformerV2-E3_D5 <<<

--- nnunet/run/run_training.py
+++ nnunet/run/run_training.py
@@ -40,7 +40,7 @@ def main():
     )
     parser.add_argument(
         "--desc",
-        default="ERNET-wDS-epoch500-baseformerV2-E3_D3",
+        default="ERNET-wDS-epoch500-baseformerV2-E3_D5",
         help="the description of method",
     )
     parser.add_argument("--network_trainer", default="nnUNetTrainerV2")

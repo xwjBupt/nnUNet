a98536dfe4c4cf1366bea29636591e2136b486d8
xwjBupt <xwj_bupt@163.com>
2023-09-17 10:07:07 +0800

    ERNet_E5_D3-D3

--- nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
+++ nnunetv2/training/nnUNetTrainer/nnUNetTrainer.py
@@ -118,7 +118,7 @@ from torch.nn.parallel import DistributedDataParallel as DDP
 import cv2
 
 
-def get_edges(targets, erode_kernel_size=7, dilate_kernel_size=5):
+def get_edges(targets, erode_kernel_size=5, dilate_kernel_size=3):
     erode_edges_all = []
     dilate_edges_all = []
     for target in targets:

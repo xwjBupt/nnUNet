bedd627b393cde8e8da02ccc171b20045ff3c8b0
xwjBupt <xwj_bupt@163.com>
2023-09-18 09:27:08 +0800

    change ERNet transformer depth to 2 dropout0

--- nnunetv2/utilities/get_network_from_plans.py
+++ nnunetv2/utilities/get_network_from_plans.py
@@ -298,9 +298,9 @@ def get_custom_network_from_plans(
                 out_ch_seg=out_classes,
                 deep_supervision=deep_supervision,
                 trans_mode="baseformerV2",
-                depth=3,
+                depth=2,
                 heads=16,
-                dropout=0.1,
+                dropout=0,
                 mlp_dim=2048,
             )
             model.apply(InitWeights_He(1e-2))
